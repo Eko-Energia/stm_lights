fb0bcabb521a79baf3563cb92cf0df6c18b910c5
lukas <a1gondek@protonmail.com>
2026-06-06 14:13:19 +0200

    Add .gitignore; stop tracking build output

--- ligths_driver/Debug/App/Eko_Drivers/Src/dma_driver.d
+++
@@ -1,3 +0,0 @@
-App/Eko_Drivers/Src/dma_driver.o: ../App/Eko_Drivers/Src/dma_driver.c \
- C:/Users/lukas/STM32CubeIDE/ligths_driver/App/Eko_Drivers/Inc/dma_driver.h
-C:/Users/lukas/STM32CubeIDE/ligths_driver/App/Eko_Drivers/Inc/dma_driver.h:
--- ligths_driver/Debug/App/Src/global_variables.d
+++
@@ -1,3 +0,0 @@
-App/Src/global_variables.o: ../App/Src/global_variables.c \
- C:/Users/lukas/STM32CubeIDE/ligths_driver/App/Inc/global_variables.h
-C:/Users/lukas/STM32CubeIDE/ligths_driver/App/Inc/global_variables.h:
